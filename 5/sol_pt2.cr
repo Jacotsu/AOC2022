ae1ab37cfec5c4e4bb0b9a606d6edea21ed84910
Jacotsu <Jacotsu@users.noreply.github.com>
2022-12-05 07:14:49 +0100

    Added day 5 solution

---
+++ 5/sol_pt2.cr
@@ -0,0 +1,31 @@
+File.open("input.txt") do |file|
+  #Read drawing
+  crates_drawing = Array(String).new
+  while true
+    line = file.read_line
+    break if line.empty?
+    crates_drawing.push line
+  end
+  n_columns = crates_drawing.pop.split.pop.to_i
+  crates_arrangment = Array(Array(Char)).new(n_columns) { |i| Array(Char).new }
+  crates_drawing.each do |line|
+    (1..line.size).step(by: 4) do |x|
+      if line[x].letter?
+        crates_arrangment[x//4].push line[x]
+      end
+    end
+  end
+  # Reverse stacks
+  crates_arrangment.each &.reverse!
+
+  # Read moves
+  file.each_line do |line|
+    split_move = line.split
+    n_crates = split_move[1].to_i
+    src = split_move[3].to_i - 1
+    dst = split_move[5].to_i - 1
+    crates_arrangment[dst] += crates_arrangment[src].pop n_crates
+  end
+  crates_arrangment.each { |x| print x.last }
+  puts
+end
