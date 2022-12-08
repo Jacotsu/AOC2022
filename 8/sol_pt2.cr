54705d1f70f91090eccc229cc1b38df422828289
Jacotsu <Jacotsu@users.noreply.github.com>
2022-12-08 22:48:21 +0100

    Made day 8 pt 2 solution more logical

--- 8/sol_pt2.cr
+++ 8/sol_pt2.cr
@@ -5,28 +5,28 @@ def calc_score(grid, i, j) : Int32
   check_visibility = ->(max_height : Int32, i : Int32, j : Int32) : Bool {
     elem = grid[i][j]
     if elem >= max_height
-      return true
-    else
       return false
+    else
+      return true
     end
   }
 
   score = Array(Int32).new 4, 0
   (j+1...size).each { |z|
     score[0] += 1
-    break if check_visibility.call cur_elem, i, z
+    break if !check_visibility.call cur_elem, i, z
   }
   (0...j).reverse_each { |z|
     score[1] += 1
-    break if check_visibility.call cur_elem, i, z
+    break if !check_visibility.call cur_elem, i, z
   }
   (i+1...size).each { |z|
     score[2] += 1
-    break if check_visibility.call cur_elem, z, j
+    break if !check_visibility.call cur_elem, z, j
   }
   (0...i).reverse_each { |z|
     score[3] += 1
-    break if check_visibility.call cur_elem, z, j
+    break if !check_visibility.call cur_elem, z, j
   }
   return score.product
 end
